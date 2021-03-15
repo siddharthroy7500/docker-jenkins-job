3f391b81cdcf4b4554f8ea12e36ba1bb90375b5b
Siddharth Roy <siddharth.roy@nagarro.com>
2021-03-16 00:12:15 +0530

    yo yo

---
+++ src/main/java/sample/application/sample_application/App.java
@@ -0,0 +1,13 @@
+package sample.application.sample_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
